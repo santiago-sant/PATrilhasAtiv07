83e4f07133ebd7d89edc7abfadf6a6fdac3d807a
Alvaro Ramos <alvarosantiagosilva03@gmail.com>
2025-02-05 08:14:25 -0300

    Create A.cs

---
+++ A.cs
@@ -0,0 +1,18 @@
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Digite um numero :");
+			int entrada1 = int.Parse(Console.ReadLine());
+			while (entrada1 > 0) {
+				Console.WriteLine("Numeros pares entre 0 e " + entrada1);
+				for( int i = 0; i <= entrada1; i += 2){
+					Console.WriteLine(i);
+			}
+			
+		
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
