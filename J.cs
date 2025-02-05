e0e0f07845522ccaabd81c52de091323ff982857
Alvaro Ramos <alvarosantiagosilva03@gmail.com>
2025-02-05 18:50:48 -0300

    Create J.cs

---
+++ J.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Crie uma senha: ");
+        string entrada1 = Console.ReadLine();
+
+        int tentativas = 0;
+        const int limite = 3;
+
+        do
+        {
+            Console.WriteLine("Informe a senha: ");
+            string senha = Console.ReadLine();
+
+            if (senha == entrada1)
+            {
+                Console.WriteLine("Acesso permitido!");
+                return; 
+            }
+            else
+            {
+                tentativas++;
+                if (tentativas < limite)
+                {
+                    Console.WriteLine($"Senha incorreta! Tentativas restantes: {limite - tentativas}");
+                }
+
+            }
+
+        } while (tentativas < limite);
+
+        Console.WriteLine("Número máximo de tentativas atingido. Acesso bloqueado!");
+    }
+}
